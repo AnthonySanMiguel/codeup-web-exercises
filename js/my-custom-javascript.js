9c48f7c3c72c6cba759aeee93bd9329e9774e646
Anthony San Miguel <asanmiguel1088@gmail.com>
2020-02-27 09:48:57 -0600

    updated jquery exercises with mouse events practices.

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,6 +2,39 @@
 
 $(document).ready(function() {
 
-        alert( 'The DOM has finished loading!' );
+    // alert('The DOM has finished loading!'); // Will create an alert on page refresh once DOM has finished loading
+
+    // alert($('#final_word').html()); // Will create an alert on page refresh that displays content of element with ID of "final_word"
+
+    // $('.codeup').css('border', '1px solid red'); // Targets all elements with class of "codeup" and adds a 1px border red
+
+    // $('li').css('font-size', '20px'); // Targets list item "li" elements and changes the font sizes for all
+
+    // $('h1, p, li').css('background-color', 'red'); // Targets multiple element types and changes the background color for all
+
+    // alert($('h1').html());; // Will create an alert on page refresh that displays content of h1 element
+
+    // $('h1').css('background-color', 'red'); // Targets h1 element and changes the background color for all
+    // $('p').css('background-color', 'yellow'); // Targets p element and changes the background color for all
+    // $('li').css('background-color', 'green'); // Targets li element and changes the background color for all
+
+    // $('li').css('font-size', '20px').css('border', '1px solid red'); // Targets list item "li" elements and changes the font sizes for all AND adds red border
+
+    // $('h1').dblclick(function(e) { // Will change the background color of all h1 elements when double-clicked
+    //     $(this).css('background-color', 'red');
+    // });
+
+    // $('p').dblclick(function(e) { // Will change the font size of all p elements when double-clicked
+    //     $(this).css('font-size', '18px');
+    // });
+
+    // $('li').hover( // Will change effect of li items to red when hovered over and black when not hovered over
+    //     function() {
+    //         $(this).css('color', 'red');
+    //     },
+    //     function() {
+    //         $(this).css('color', 'black');
+    //     }
+    // );
 
 });
