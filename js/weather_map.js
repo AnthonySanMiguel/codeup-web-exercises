6c6e5009c015cca04ad9f781c1555ac8c5e03b17
Anthony San Miguel <asanmiguel1088@gmail.com>
2020-03-09 10:07:49 -0500

    weather map functionality updated

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,73 +1,72 @@
 "use strict";
 
+mapboxgl.accessToken = mapboxToken; // Variable linking Mapbox API token
+
+var map = new mapboxgl.Map({ // Mapbox API map styling and location origin
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-98.4936, 29.4241], // Mapbox API will generate a map based on the latitude and longitude retrieved from the DarkSky API ajax call
+    zoom: 11
+});
+
+map.addControl( // Mapbox API geocoder to retrieve coordinates from text input in search box
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        marker: {
+            draggable: true,
+            color: 'green'
+        },
+        mapboxgl: mapboxgl
+    })
+);
+
+var marker = new mapboxgl.Marker({ // Mapbox API marker customization
+    draggable: true,
+    color: "red"
+})
+    .setLngLat([-98.4936, 29.4241])
+    .addTo(map);
+
+var coordinates = document.getElementById('coordinates'); // Mapbox API variable for reversegeocode function
+
+function onDragEnd(lat, lng) { // Mapbox API displays marker coordinates on bottom left of map after drag
+    var lngLat = marker.getLngLat();
+    coordinates.style.display = 'block';
+    coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br>Latitude: ' + lngLat.lat;
+}
+
 $(document).ready(function (coordinates, token) {
 
-    getWeather(29.4241, -98.4936); // Using DarkSky API
+    // getWeather(29.4241, -98.4936); // Using DarkSky API
 
     function getWeather(latitude, longitude) {
-        $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/" + latitude + "," + longitude).done(function (data) {
+        $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/" + marker.getLngLat().lat + "," + marker.getLngLat().lng).done(function (data) {
             console.log(data); // Will retrieve DarkSky API object with weather information for coordinates hard-coded into "getWeather" function
 
             makeDay1(data); // Generates selected weather info for three days
             makeDay2(data);
             makeDay3(data);
 
-            var latitudeName = data.latitude; // Setting DarkSky API latitude as a variable
-            var longitudeName = data.longitude; // Setting DarkSky API longitude as a variable
+            marker.on('dragend', getWeather); // At end of marker drag interaction ('dragend'), run the onDragEnd function
 
-            mapboxgl.accessToken = mapboxToken; // Variable linking Mapbox API token
+            // var latitudeName = data.latitude; // Setting DarkSky API latitude as a variable
+            // var longitudeName = data.longitude; // Setting DarkSky API longitude as a variable
 
-            var map = new mapboxgl.Map({ // Mapbox API map styling and location origin
-                container: 'map',
-                style: 'mapbox://styles/mapbox/streets-v9',
-                center: [longitudeName, latitudeName], // Mapbox API will generate a map based on the latitude and longitude retrieved from the DarkSky API ajax call
-                zoom: 11
-            });
-
-            map.addControl( // Mapbox API geocoder to retrieve coordinates from text input in search box
-                new MapboxGeocoder({
-                    accessToken: mapboxgl.accessToken,
-                    marker: {
-                        draggable: true,
-                        color: 'green'
-                    },
-                    mapboxgl: mapboxgl
-                })
-            );
-
-            var marker = new mapboxgl.Marker({ // Mapbox API marker customization
-                draggable: true,
-                color: "green"
-            })
-                .setLngLat([longitudeName, latitudeName])
-                .addTo(map);
-
-            var coordinates = document.getElementById('coordinates'); // Mapbox API variable for reversegeocode function
-
-            function onDragEnd() { // Mapbox API displays marker coordinates on bottom left of map after drag
-                var lngLat = marker.getLngLat();
-                coordinates.style.display = 'block';
-                coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br>Latitude: ' + lngLat.lat;
-            }
-
-            marker.on('dragend', onDragEnd); // At end of marker drag interaction ('dragend'), run the onDragEnd function
-
-            var addressName = reverseGeocode({
-                lng: data.longitude,
-                lat: data.latitude
-            }, mapboxgl.accessToken).then(function (results) { // Retrieve address from coordinates
-                console.log(results);
-            });
-
-            //update your stuff
-
-            $("#location_name").html( // Attempting to display address text from coordinates
-                "<div>" +
-                addressName +
-                "</div>");
+            // var addressName = reverseGeocode({ // Retrieve address from coordinates
+            //     lng: data.longitude,
+            //     lat: data.latitude
+            // }, mapboxgl.accessToken).then(function (results) {
+            //     console.log(results);
+            // });
+            //
+            // $("#location_name").html( // Attempting to display address text from coordinates
+            //     "<div>" +
+            //     addressName +
+            //     "</div>");
 
         });
     }
+    getWeather();
 
     function makeDay1(data) {
 
@@ -110,5 +109,4 @@ $(document).ready(function (coordinates, token) {
             "<p>" + "<strong>" + new Date(data.daily.data[2].time * 1000).toDateString() + "</strong>" + "</p>" +
             "</div>");
     }
-
 });
